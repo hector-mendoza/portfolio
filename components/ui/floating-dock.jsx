2dd0d22723de9b6447105d054d0fcf28445f4a6e
Hector <hey@hectormendoza.me>
2026-02-05 10:08:14 -0600

    Update floating-dock.jsx

--- components/ui/floating-dock.jsx
+++ components/ui/floating-dock.jsx
@@ -69,7 +69,7 @@ const FloatingDockDesktop = ({
       onMouseMove={(e) => mouseX.set(e.pageX)}
       onMouseLeave={() => mouseX.set(Infinity)}
       className={cn(
-        "mx-auto hidden md:flex h-16 gap-4 items-end  rounded-2xl bg-gray-50 dark:bg-neutral-900 px-4 pb-3 shadow-sm",
+        "mx-auto hidden md:flex h-16 gap-4 items-end  rounded-2xl bg-gray-50/10 dark:bg-neutral-900 px-4 pb-3 shadow-sm animate-float",
         className
       )}>
       {items.map((item) => (
